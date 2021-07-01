3b5114f89347d6e8612517d182553a709087175c
Manu <mcufre84@gmail.com>
2021-07-01 20:03:50 +0200

    fixed heading alignment services

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 <div class="jumbotron" style="padding:10px; text-align:center">
-    
+
     <h1>Services</h1>
     </div>
 @if(count($services) > 0)
-      
+
 <ul class="list-group mb-5">
     @foreach($services as $service)
-        <li class="list-group-item "><h4>{{$service}}</h4></li>
-       
+        <li class="list-group-item " style="text-align: center"><h4>{{$service}}</h4></li>
+
             <img src={{$images[ $loop->index ]}} class="mb-3" style="height: 600px" />
         @endforeach
-  
+
 </ul>
 
 @endif
